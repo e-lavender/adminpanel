a715489db3e07d79aa8cbe29c338eca409b9a2f8
Elena1789 <elenadubovskaya8@gmail.com>
2024-04-14 16:37:17 +0200

    feat: header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 
 import '@/styles/index.css'
+import '@flyingtornado06/ui-kit/dist/style.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ src/pages/auth/sign-in/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Header } from '@/widgets/Header/Header'
+
+const SignInPage = () => {
+  return <Header />
+}
+
+export default SignInPage
