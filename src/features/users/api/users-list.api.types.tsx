d33a78b530a522c58bbc26d698bc86828c79052c
jalxby <jalxby@gmail.com>
2024-06-17 21:33:14 +0200

    feat: user uploadedPhotos

--- src/features/users/api/users-list.api.types.tsx
+++ src/features/users/api/users-list.api.types.tsx
@@ -1,97 +1,58 @@
-import { gql } from '@apollo/client'
-import * as Apollo from '@apollo/client'
+import * as Types from '../../../shared/appolo-client/Schema.types';
 
-import * as Types from '../../../shared/appolo-client/Schema.types'
-const defaultOptions = {} as const
-
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+const defaultOptions = {} as const;
 export type GetAllUsersQueryVariables = Types.Exact<{
-  pageNumber: Types.Scalars['Int']
-  pageSize: Types.Scalars['Int']
-  searchTerm: Types.Scalars['String']
-  sortBy: Types.Scalars['String']
-  sortDirection: Types.SortDirection
-  statusFilter?: Types.UserBlockStatus
-}>
+  pageSize: Types.Scalars['Int'];
+  pageNumber: Types.Scalars['Int'];
+  sortBy: Types.Scalars['String'];
+  sortDirection: Types.SortDirection;
+  searchTerm: Types.Scalars['String'];
+  statusFilter?: Types.UserBlockStatus;
+}>;
 
-export type GetAllUsersQuery = {
-  __typename?: 'Query'
-  getUsers: {
-    __typename?: 'UsersPaginationModel'
-    pagination: {
-      __typename?: 'PaginationModel'
-      page: number
-      pageSize: number
-      pagesCount: number
-      totalCount: number
-    }
-    users: Array<{
-      __typename?: 'User'
-      profile: {
-        __typename?: 'Profile'
-        aboutMe?: null | string
-        avatars?: Array<{
-          __typename?: 'Avatar'
-          fileSize?: null | number
-          height?: null | number
-          url?: null | string
-          width?: null | number
-        }> | null
-        city?: null | string
-        createdAt: any
-        dateOfBirth?: any | null
-        firstName?: null | string
-        id: number
-        lastName?: null | string
-        userName?: null | string
-      }
-    }>
-  }
-}
+
+export type GetAllUsersQuery = { __typename?: 'Query', getUsers: { __typename?: 'UsersPaginationModel', pagination: { __typename?: 'PaginationModel', page: number, pagesCount: number, pageSize: number, totalCount: number }, users: Array<{ __typename?: 'User', profile: { __typename?: 'Profile', createdAt: any, aboutMe?: string | null, city?: string | null, dateOfBirth?: any | null, id: number, lastName?: string | null, firstName?: string | null, userName?: string | null, avatars?: Array<{ __typename?: 'Avatar', fileSize?: number | null, height?: number | null, url?: string | null, width?: number | null }> | null } }> } };
+
 
 export const GetAllUsersDocument = gql`
-  query getAllUsers(
-    $pageSize: Int!
-    $pageNumber: Int!
-    $sortBy: String!
-    $sortDirection: SortDirection!
-    $searchTerm: String!
-    $statusFilter: UserBlockStatus! = ALL
+    query getAllUsers($pageSize: Int!, $pageNumber: Int!, $sortBy: String!, $sortDirection: SortDirection!, $searchTerm: String!, $statusFilter: UserBlockStatus! = ALL) {
+  getUsers(
+    pageSize: $pageSize
+    pageNumber: $pageNumber
+    sortBy: $sortBy
+    sortDirection: $sortDirection
+    searchTerm: $searchTerm
+    statusFilter: $statusFilter
   ) {
-    getUsers(
-      pageSize: $pageSize
-      pageNumber: $pageNumber
-      sortBy: $sortBy
-      sortDirection: $sortDirection
-      searchTerm: $searchTerm
-      statusFilter: $statusFilter
-    ) {
-      pagination {
-        page
-        pagesCount
-        pageSize
-        totalCount
-      }
-      users {
-        profile {
-          createdAt
-          aboutMe
-          city
-          dateOfBirth
-          id
-          lastName
-          firstName
-          userName
-          avatars {
-            fileSize
-            height
-            url
-            width
-          }
+    pagination {
+      page
+      pagesCount
+      pageSize
+      totalCount
+    }
+    users {
+      profile {
+        createdAt
+        aboutMe
+        city
+        dateOfBirth
+        id
+        lastName
+        firstName
+        userName
+        avatars {
+          fileSize
+          height
+          url
+          width
         }
       }
     }
   }
-`
+}
+    `;
 
 /**
  * __useGetAllUsersQuery__
@@ -114,23 +75,14 @@ export const GetAllUsersDocument = gql`
  *   },
  * });
  */
-export function useGetAllUsersQuery(
-  baseOptions: Apollo.QueryHookOptions<GetAllUsersQuery, GetAllUsersQueryVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions }
-
-  return Apollo.useQuery<GetAllUsersQuery, GetAllUsersQueryVariables>(GetAllUsersDocument, options)
-}
-export function useGetAllUsersLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<GetAllUsersQuery, GetAllUsersQueryVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions }
-
-  return Apollo.useLazyQuery<GetAllUsersQuery, GetAllUsersQueryVariables>(
-    GetAllUsersDocument,
-    options
-  )
-}
-export type GetAllUsersQueryHookResult = ReturnType<typeof useGetAllUsersQuery>
-export type GetAllUsersLazyQueryHookResult = ReturnType<typeof useGetAllUsersLazyQuery>
-export type GetAllUsersQueryResult = Apollo.QueryResult<GetAllUsersQuery, GetAllUsersQueryVariables>
+export function useGetAllUsersQuery(baseOptions: Apollo.QueryHookOptions<GetAllUsersQuery, GetAllUsersQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetAllUsersQuery, GetAllUsersQueryVariables>(GetAllUsersDocument, options);
+      }
+export function useGetAllUsersLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetAllUsersQuery, GetAllUsersQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetAllUsersQuery, GetAllUsersQueryVariables>(GetAllUsersDocument, options);
+        }
+export type GetAllUsersQueryHookResult = ReturnType<typeof useGetAllUsersQuery>;
+export type GetAllUsersLazyQueryHookResult = ReturnType<typeof useGetAllUsersLazyQuery>;
+export type GetAllUsersQueryResult = Apollo.QueryResult<GetAllUsersQuery, GetAllUsersQueryVariables>;
