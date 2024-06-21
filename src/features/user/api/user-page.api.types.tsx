fdda71968e388773e8590669f40b3f30f40af41c
Elena1789 <elenadubovskaya8@gmail.com>
2024-06-21 22:29:26 +0200

    feat: payments query added

--- src/features/user/api/user-page.api.types.tsx
+++ src/features/user/api/user-page.api.types.tsx
@@ -18,6 +18,17 @@ export type GetAllPostsByUserQueryVariables = Types.Exact<{
 
 export type GetAllPostsByUserQuery = { __typename?: 'Query', getPostsByUser: { __typename?: 'PostsByUserModel', totalCount: number, pageSize: number, pagesCount: number, items?: Array<{ __typename?: 'ImagePost', id?: number | null, createdAt?: any | null, url?: string | null, width?: number | null, height?: number | null, fileSize?: number | null }> | null } };
 
+export type GetPaymentsByIdQueryVariables = Types.Exact<{
+  userId: Types.Scalars['Int'];
+  pageSize?: Types.InputMaybe<Types.Scalars['Int']>;
+  pageNumber?: Types.InputMaybe<Types.Scalars['Int']>;
+  sortBy?: Types.InputMaybe<Types.Scalars['String']>;
+  sortDirection?: Types.InputMaybe<Types.SortDirection>;
+}>;
+
+
+export type GetPaymentsByIdQuery = { __typename?: 'Query', getPaymentsByUser: { __typename?: 'PaymentPaginationModel', page: number, pagesCount: number, pageSize: number, totalCount: number, items: Array<{ __typename?: 'SubscriptionByPaymentModel', dateOfPayment?: any | null, endDate?: any | null, price: number, paymentType?: Types.PaymentMethod | null, type: Types.SubscriptionType }> } };
+
 
 export const GetUserByIdDocument = gql`
     query getUserById($userId: Int!) {
@@ -112,3 +123,58 @@ export function useGetAllPostsByUserLazyQuery(baseOptions?: Apollo.LazyQueryHook
 export type GetAllPostsByUserQueryHookResult = ReturnType<typeof useGetAllPostsByUserQuery>;
 export type GetAllPostsByUserLazyQueryHookResult = ReturnType<typeof useGetAllPostsByUserLazyQuery>;
 export type GetAllPostsByUserQueryResult = Apollo.QueryResult<GetAllPostsByUserQuery, GetAllPostsByUserQueryVariables>;
+export const GetPaymentsByIdDocument = gql`
+    query getPaymentsById($userId: Int!, $pageSize: Int = 10, $pageNumber: Int = 1, $sortBy: String = "createdAt", $sortDirection: SortDirection = desc) {
+  getPaymentsByUser(
+    userId: $userId
+    pageSize: $pageSize
+    pageNumber: $pageNumber
+    sortBy: $sortBy
+    sortDirection: $sortDirection
+  ) {
+    items {
+      dateOfPayment
+      endDate
+      price
+      paymentType
+      type
+    }
+    page
+    pagesCount
+    pageSize
+    totalCount
+  }
+}
+    `;
+
+/**
+ * __useGetPaymentsByIdQuery__
+ *
+ * To run a query within a React component, call `useGetPaymentsByIdQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetPaymentsByIdQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetPaymentsByIdQuery({
+ *   variables: {
+ *      userId: // value for 'userId'
+ *      pageSize: // value for 'pageSize'
+ *      pageNumber: // value for 'pageNumber'
+ *      sortBy: // value for 'sortBy'
+ *      sortDirection: // value for 'sortDirection'
+ *   },
+ * });
+ */
+export function useGetPaymentsByIdQuery(baseOptions: Apollo.QueryHookOptions<GetPaymentsByIdQuery, GetPaymentsByIdQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetPaymentsByIdQuery, GetPaymentsByIdQueryVariables>(GetPaymentsByIdDocument, options);
+      }
+export function useGetPaymentsByIdLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetPaymentsByIdQuery, GetPaymentsByIdQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetPaymentsByIdQuery, GetPaymentsByIdQueryVariables>(GetPaymentsByIdDocument, options);
+        }
+export type GetPaymentsByIdQueryHookResult = ReturnType<typeof useGetPaymentsByIdQuery>;
+export type GetPaymentsByIdLazyQueryHookResult = ReturnType<typeof useGetPaymentsByIdLazyQuery>;
+export type GetPaymentsByIdQueryResult = Apollo.QueryResult<GetPaymentsByIdQuery, GetPaymentsByIdQueryVariables>;
