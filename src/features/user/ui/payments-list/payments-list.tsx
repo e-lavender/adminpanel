9ee9a6af72babd121e07951982ef389d1b522330
Elena1789 <elenadubovskaya8@gmail.com>
2024-06-22 21:48:49 +0200

    feat: payments query data

--- src/features/user/ui/payments-list/payments-list.tsx
+++ src/features/user/ui/payments-list/payments-list.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { PAYMENTS_TABLE_COLUMNS } from '@/features/user/constants'
 import { PaymentsListStyled } from '@/features/user/ui/payments-list/payments-list.styled'
 import { UsersTable } from '@/features/users/ui/users-table'
+import { SortDirection } from '@/shared/appolo-client/Schema.types'
+import { useGetPaymentsByIdQuery } from '@/features/user/api/user-page.api.types'
+import { useRouter } from 'next/router'
 
 export const PaymentsList = () => {
+  const [pageNumber, setPageNumber] = useState<number>(1)
+  const [pageSize, setPageSize] = useState<number>(10)
+
   const { Table } = PaymentsListStyled
+  const { query } = useRouter()
+  const userId = Number(query.id && query.id[0])
+  const { data, loading } = useGetPaymentsByIdQuery({
+    skip: !userId,
+    variables: {
+      pageNumber,
+      userId,
+      pageSize,
+      sortBy: 'createdAt',
+      sortDirection: SortDirection.Asc,
+    },
+  })
 
   // @ts-ignore
   const table = <UsersTable columns={PAYMENTS_TABLE_COLUMNS} data={''} />
