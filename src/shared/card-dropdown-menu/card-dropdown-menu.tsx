c6d89b0df95b642a9c29bda40f6c411309455e00
Elena1789 <elenadubovskaya8@gmail.com>
2024-07-27 21:08:56 +0200

    feat: menu for dropdown menu

--- src/shared/card-dropdown-menu/card-dropdown-menu.tsx
+++ src/shared/card-dropdown-menu/card-dropdown-menu.tsx
@@ -1,11 +1,10 @@
-import { toast } from 'react-toastify'
-
 import { ConfirmationModal } from '@/shared/card-dropdown-menu/confirmation-modal'
 import { useDisclose } from '@/shared/hooks/useDisclose'
 import { DropdownMenu } from '@/ui/common/dropdown-menu'
 import { MenuItem } from '@/ui/common/menu-item'
+import { DropdownMenuType, MENU } from '@/shared/card-dropdown-menu/data'
 
-export const CardDropdownMenu = ({ id }: DropDownMenuType) => {
+export const CardDropdownMenu = (Props: DropdownMenuType) => {
   const { isOpen: isModalOpened, onClose: closeModal, onOpen: openModal } = useDisclose()
   const { isOpen: isControlled, onToggle: closeDropdownMenu } = useDisclose(true)
 
@@ -31,7 +30,7 @@ export const CardDropdownMenu = ({ id }: DropDownMenuType) => {
   return (
     <>
       <DropdownMenu>
-        <MenuItem as={'button'} onClick={''} />
+        {MENU?.map(item => <MenuItem as={'button'} onClick={''} key={item.action} />)}
       </DropdownMenu>
 
       <ConfirmationModal
--- src/shared/card-dropdown-menu/data/index.ts
+++ src/shared/card-dropdown-menu/data/index.ts
@@ -1,33 +1,30 @@
 import { HomeMenuIcon } from '@flyingtornado06/ui-kit/dist/assets'
 
-export const MENU: DropdownMenuType = {
-   [
-    {
-      action: 'delete',
-      icon: '',
-      label: 'Delete User',
-    },
-    {
-      action: 'ban',
-      icon: '',
-      label: 'Ban in the system',
-    },
-    {
-      action: 'more info',
-      icon: '',
-      label: 'More Information',
-    },
-  ],
-}
+export const MENU: DropdownMenuType = [
+  {
+    action: 'delete',
+    icon: undefined,
+    label: 'Delete User',
+  },
+  {
+    action: 'ban',
+    icon: undefined,
+    label: 'Ban in the system',
+  },
+  {
+    action: 'info',
+    icon: undefined,
+    label: 'More Information',
+  },
+]
 
 export type DropdownMenuItemType = {
   action: keyof typeof ActionTypes
-  icon: SVGIconType
+  icon: SVGIconType | undefined
   label: string
 }
-export type DropdownMenuType = {
-  []: Array<DropdownMenuItemType>
-}
+export type DropdownMenuType = Array<DropdownMenuItemType>
+
 export const ActionTypes = {
   copy: 'copy',
   delete: 'delete',
@@ -35,6 +32,8 @@ export const ActionTypes = {
   follow: 'follow',
   report: 'report',
   unfollow: 'unfollow',
+  ban: 'ban',
+  info: 'info',
 } as const
 
 export type SVGIconType = typeof HomeMenuIcon
