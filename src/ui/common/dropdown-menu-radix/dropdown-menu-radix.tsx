6026709b86a5cdfe713a598bda214723dd61923b
Elena Dubovskaia <elenadubovskaya8@gmail.com>
2024-08-04 10:59:23 +0200

    ref: menu for dropdown menu (#37)

---
+++ src/ui/common/dropdown-menu-radix/dropdown-menu-radix.tsx
@@ -0,0 +1,43 @@
+import { Dots } from '@/app/assets/svg/dots-icon'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+
+export const DropdownMenuRadix = () => (
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger asChild>
+      <button>
+        <Dots />
+      </button>
+    </DropdownMenu.Trigger>
+
+    <DropdownMenu.Portal>
+      <DropdownMenu.Content>
+        <DropdownMenu.Label />
+        <DropdownMenu.Item />
+
+        <DropdownMenu.Group>
+          <DropdownMenu.Item />
+        </DropdownMenu.Group>
+
+        <DropdownMenu.CheckboxItem>
+          <DropdownMenu.ItemIndicator />
+        </DropdownMenu.CheckboxItem>
+
+        <DropdownMenu.RadioGroup>
+          <DropdownMenu.RadioItem>
+            <DropdownMenu.ItemIndicator />
+          </DropdownMenu.RadioItem>
+        </DropdownMenu.RadioGroup>
+
+        <DropdownMenu.Sub>
+          <DropdownMenu.SubTrigger />
+          <DropdownMenu.Portal>
+            <DropdownMenu.SubContent />
+          </DropdownMenu.Portal>
+        </DropdownMenu.Sub>
+
+        <DropdownMenu.Separator />
+        <DropdownMenu.Arrow />
+      </DropdownMenu.Content>
+    </DropdownMenu.Portal>
+  </DropdownMenu.Root>
+)
