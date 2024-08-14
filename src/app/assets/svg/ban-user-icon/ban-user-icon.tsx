a52755f9d95663a8b0418f748f3b9a10816f5367
Elena1789 <elenadubovskaya8@gmail.com>
2024-08-14 22:05:04 +0200

    feat: ban and delete user icons

--- src/app/assets/svg/ban-user-icon/ban-user-icon.tsx
+++ src/app/assets/svg/ban-user-icon/ban-user-icon.tsx
@@ -3,14 +3,14 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) =
   <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" ref={ref} {...props}>
     <g clipPath="url(#a)">
       <path
-        fill="#000"
+        fill="currentColor"
         d="M12 2a10 10 0 1 0 0 20 10 10 0 0 0 0-20Zm0 18a8 8 0 1 1 0-16.001A8 8 0 0 1 12 20Z"
       />
       <path stroke="#000" strokeWidth={2.3} d="m7.043 19.362 10-15" />
     </g>
     <defs>
       <clipPath id="a">
-        <path fill="#fff" d="M0 0h24v24H0z" />
+        <path fill="currentColor" d="M0 0h24v24H0z" />
       </clipPath>
     </defs>
   </svg>
